ae12ab22e4113ff22435b7e7c702079fa84cf122
Emre Sokullu <emre@groups-inc.com>
2017-01-27 18:55:39 +0300

    client with riot

--- public/assets/js/pho.js
+++ public/assets/js/pho.js
@@ -1,4 +1,4 @@
-var Pho = (function() {
+var Pho = function(app_id) {
   var _loadJS = function(url, type, implementationCode, location){
     //url is URL of external file, implementationCode is the code
     //to be called from the file, location is the location to
@@ -12,24 +12,35 @@ var Pho = (function() {
     scriptTag.onreadystatechange = implementationCode;
 
     location.appendChild(scriptTag);
+  };
+
+
+  var initializeScripts = function() {
+    console.log("Pho-username tag loaded");
+    Parse.initialize(app_id/*"myAppId" , "JAVASCRIPT_KEY"*/);
+    Parse.serverURL = "https://mealcounter-parse.herokuapp.com/parse";
+
+    var user = new Parse.User.current();
+    console.log("username is "+user.getUsername())
+    riot.mount('pho-username', { parseuser: user })
+  }
+
+  var loadTags = function() {
+    console.log("Riot compiler loaded");
+    console.log("Loading Pho-username tag");
+    _loadJS("public/assets/js/pho-username.tag", "riot/tag", undefined, document.body);
+    initializeScripts();
+  }
+
+  var loadRiot = function() {
+    console.log("Parse JS loaded");
+    console.log("Loading Riot compiler");
+    _loadJS("http://rawgit.com/riot/riot/master/riot%2Bcompiler.min.js", "text/javascript", loadTags, document.body);
+  }
+
+  var init() {
+    console.log("loading Parse JS");
+    _loadJS('http://www.parsecdn.com/js/parse-latest.js', "text/javascript", loadRiot, document.body);
+  }
+
 };
-
-
-var initialize = function() {
-  Parse.initialize("myAppId" /*, "JAVASCRIPT_KEY"*/);
-  Parse.serverURL = "https://mealcounter-parse.herokuapp.com/parse";
-
-  var user = new Parse.User.current();
-  riot.mount('pho-username', { parseuser: user })
-}
-
-var loadRiot = function() {
-  _loadJS("http://rawgit.com/riot/riot/master/riot%2Bcompiler.min.js", "text/javascript", initialize, document.body);
-}
-
-var loadTags = function() {
-  _loadJS("public/assets/js/pho-username.tag", "riot/tag", loadRiot, document.body);
-}
-
-_loadJS('http://www.parsecdn.com/js/parse-latest.js', "text/javascript", loadTags, document.body);
-});
